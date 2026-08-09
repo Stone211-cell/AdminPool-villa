be299adc11eed6d5d2b5dd1d4231d2946d878d8f
Stone211-cell <suvapat1905@gmail.com>
2026-08-09 21:34:36 +0700

    fix build: remove scratch

--- scratch/truncate.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../lib/prisma';
-
-async function main() {
-  console.log('Truncating tables...');
-  await prisma.$executeRawUnsafe('TRUNCATE TABLE houses CASCADE');
-  console.log('Done');
-}
-
-main().catch(console.error).finally(() => process.exit(0));
